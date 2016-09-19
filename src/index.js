061a20c86ca1e9bce69a7692cfc0dea4409faa70
JoseBoga <joseboga16@gmail.com>
2016-09-18 18:41:56 -0600

    add features multiplier value

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 
 //function generate Ids
 
-function generate() {
-	return Math.floor(Math.random() * 999999) + 1;
+function generate(value) {
+	if (value) {
+		value = value || 999999;
+		return Math.floor(Math.random() * value) + 1;
+	}else {
+		return Math.floor(Math.random() * 999999) + 1;
+	}
 }
 
 module.exports = generate;
